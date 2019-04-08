c177a41fb84da2c85c7bbdd85455d4ed3df2e4c6
AalaaGhanam <aalaaghanam41@hotmail.com>
2019-04-08 23:52:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const path = require("path");
+
+//Init app
 const app = express();
 
+//Load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
+
+
+//Home route
 app.get('/', function(req, res){
     res.send("sssssssss");
 });
 
+//Start server
 app.listen(3000, function(){
     console.log("Server started on port 3000....");
 });
