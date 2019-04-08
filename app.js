55ec86b6ac0b7aea94d5170df6824acd62b4df66
AalaaGhanam <aalaaghanam41@hotmail.com>
2019-04-09 00:37:35 +0200

    Initial commit

--- app.js
+++ app.js
@@ -11,6 +11,52 @@ app.set('view engine', 'pug')
 
 //Home route
 app.get('/', function(req, res){
+    let products= [{
+        "name": "P50",
+        "price": 2000,
+        "currency": "USD",
+        "cateogiry": "electornics",
+        "vendor": "lenovo",
+        "publisher": "compume",
+        "quantity": 10,
+        "description": "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias"
+    },
+    {
+        "name": "iPhone XS Max",
+        "price": 17000,
+        "currency": "EGP",
+        "cateogiry": "phones",
+        "vendor": "apple",
+        "publisher": "tradeline",
+        "quantity": 33,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+    },
+    {
+        "name": "Revenger-X",
+        "price": 40,
+        "currency": "EUR",
+        "cateogiry": "electornics",
+        "vendor": "vaporesso",
+        "publisher": "hub life",
+        "quantity": 3,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+    },
+    {
+        "name": "Reclining Sofa",
+        "price": 1139,
+        "currency": "USD",
+        "cateogiry": "furniture",
+        "vendor": "La-Z-Boy",
+        "publisher": "jumia",
+        "quantity": 12,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+    }
+];
+    res.send( products);
+});
+
+//Add all products
+app.get('/addAllProducts', function(req, res){
     res.send("sssssssss");
 });
 
