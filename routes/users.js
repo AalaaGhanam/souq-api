5ddb6ec872e8d42aa219f25788adbf2501b2cb50
AalaaGhanam <aalaaghanam41@hotmail.com>
2019-04-10 11:35:55 +0200

    small fix

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const bodyParser = require('body-parser');
-const { test, login } = require('../controllers/users');
+const { login } = require('../controllers/users');
 
 router.use(bodyParser.json());
 
 router.route('/login')
     .post(login)
-    
-router.route('/test')
-    .post(test)
 
 module.exports = router;
